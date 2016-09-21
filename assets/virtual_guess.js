400654f589f573f47ea9007c490b49393787f59f
Joshua Yang <tianjyang@gmail.com>
2016-09-20 18:27:45 -0700

    refactoring sensor method because it is too computationally intense for javascript

--- assets/virtual_guess.js
+++ assets/virtual_guess.js
@@ -13,42 +13,42 @@ class VirtualGuess extends createjs.Shape {
     return this;
   }
 
-  takeMeasurement() {
-    let startingPoint, distance;
-    let index = 0
-    startingPoint = [this.x,this.y];
-    for (let i = 0; i <= 360; i+=90) {
-      startingPoint = [this.x,this.y];
-      distance = this.takeSensorReading(startingPoint,i);
-      this.measurement[index] = distance;
-      index += 1;
-    }
-  }
+  // takeMeasurement() {
+  //   let startingPoint, distance;
+  //   let index = 0
+  //   startingPoint = [this.x,this.y];
+  //   for (let i = 0; i <= 360; i+=90) {
+  //     startingPoint = [this.x,this.y];
+  //     distance = this.takeSensorReading(startingPoint,i);
+  //     this.measurement[index] = distance;
+  //     index += 1;
+  //   }
+  // }
 
-  takeSensorReading(startingPoint,angle) {
-    const thisContext = this;
-    let radians = angle*Math.PI/180;
-    let startPoint = startingPoint;
-    let endPoint = startingPoint.slice(0);
-    let keepLooping = true;
-    const colorAtPix = (x,y) => {
-      return Array.from(thisContext.stageVar.canvas.getContext('2d').getImageData(x,y,5,5).data);
-    };
-
-    const isBlack = (rgbArray) => {
-      rgbArray.pop();
-      return rgbArray.every((el)=>{
-        return el === 0;
-      });
-    };
-    while (keepLooping) {
-      endPoint = Util.stepInDirectionDegs(endPoint,radians,10);
-      keepLooping = !Util.hasBlack(colorAtPix(endPoint[0],endPoint[1]));
-    }
-    let vector = Util.vectorBetweenCenters(startPoint,endPoint);
-    return Util.vectorMagnitude(vector);
-
-  }
+  // takeSensorReading(startingPoint,angle) {
+  //   const thisContext = this;
+  //   let radians = angle*Math.PI/180;
+  //   let startPoint = startingPoint;
+  //   let endPoint = startingPoint.slice(0);
+  //   let keepLooping = true;
+  //   const colorAtPix = (x,y) => {
+  //     return Array.from(thisContext.stageVar.canvas.getContext('2d').getImageData(x,y,5,5).data);
+  //   };
+  //
+  //   const isBlack = (rgbArray) => {
+  //     rgbArray.pop();
+  //     return rgbArray.every((el)=>{
+  //       return el === 0;
+  //     });
+  //   };
+  //   while (keepLooping) {
+  //     endPoint = Util.stepInDirectionDegs(endPoint,radians,10);
+  //     keepLooping = !Util.hasBlack(colorAtPix(endPoint[0],endPoint[1]));
+  //   }
+  //   let vector = Util.vectorBetweenCenters(startPoint,endPoint);
+  //   return Util.vectorMagnitude(vector);
+  //
+  // }
 
   drawSelf() {
     this.graphics.beginFill("rgba(255,0,0,0.05)").drawCircle(0,0,20);
